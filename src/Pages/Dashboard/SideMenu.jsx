abc4b1694e64945bc4b3c0357474675b11912883
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2024-03-15 13:50:39 +0530

    404 page added and bugs fixed

--- src/Pages/Dashboard/SideMenu.jsx
+++ src/Pages/Dashboard/SideMenu.jsx
@@ -33,7 +33,7 @@ const SideMenu = () => {
     return (
 
         <div className="fixed">
-            <aside className="w-56 h-screen flex justify-start items-center gap-2 text-sm flex-col  font-semibold shadow-xl"data-aos="fade-right">
+            <aside className="w-56 h-screen flex justify-start items-center gap-2 text-sm flex-col  font-semibold shadow-xl" data-aos="fade-right">
                 <div className="mt-10  border-b-2 w-full flex justify-center items-center py-4 border-gray-400">
 
 
@@ -52,13 +52,17 @@ const SideMenu = () => {
                         <div onClick={() => setIsCourseOpen(!isCourseOpen)} className="flex justify-between items-center p-2 hover:text-[#B32073] hover:bg-gray-300">
                             <div className="flex justify-center items-center gap-2">
                                 <p><FaBook /></p>
-                                <h1 className="text-xl text-gray-500 cursor-pointer hover:text-[#B32073]">Courses</h1>
+                                <h1 className="text-xl text-gray-500 cursor-pointer hover:text-[#B32073]" >Courses</h1>
                             </div>
                             <p><FaChevronDown /></p>
                         </div>
                         <div className={`sm ${isCourseOpen ? "flex" : "hidden"} flex-col gap-5 mt-5`}>
-                            <NavLink to="/Courses/Categories" className={({ isActive, isPending }) => isPending ? "flex p-2 flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? "flex flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl" : "text-gray-500"}><p className="hover:text-[#B32073] w-full p-3 text-gray-500">Categories</p></NavLink>
-                            <NavLink to="/Courses/CourseList" className={({ isActive, isPending }) => isPending ? "flex p-2 flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? "flex flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl" : "text-gray-500"}><p className="hover:text-[#B32073] w-full p-3 text-gray-500">Course List</p></NavLink>
+                            <NavLink to="/Courses/Categories" className={({ isActive, isPending }) => isPending ? "flex p-2 text-xl flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? `flex text-xl flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl ${setIsCourseOpen(true)}`: "text-gray-500"}>
+                                <p className="hover:text-[#B32073] w-full p-2 ">Categories</p>
+                            </NavLink>
+                            <NavLink to="/Courses/CourseList" className={({ isActive, isPending }) => isPending ? "flex text-xl p-2 flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? `flex text-xl flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl ${setIsCourseOpen(true)}` : "text-gray-500"}>
+                                <p className="hover:text-[#B32073] w-full p-2 ">Course List</p>
+                            </NavLink>
                         </div>
                     </div>
 
@@ -73,8 +77,8 @@ const SideMenu = () => {
                         <div className={`sm ${isAssignmentOpen ? "flex" : "hidden"} flex-col gap-2 mt-1`}>
 
                             {/* <p >Assignment</p> */}
-                            <NavLink to="/Assessment/Record" className={({ isActive, isPending }) => isPending ? "flex p-2 flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? "flex flex-row items-center duration-300 scale-105  bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl" : "text-gray-500"}><p className="hover:text-[#B32073] w-full p-3 rounded hover:bg-gray-300 text-gray-500">Record</p></NavLink>
-                            <NavLink to="/Assessment/List" className={({ isActive, isPending }) => isPending ? "flex hover:text-[#B32073] flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? "flex flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl" : "text-gray-500"}><p className="hover:text-[#B32073] w-full p-3 rounded hover:bg-gray-300 text-gray-500">Question List</p></NavLink>
+                            <NavLink to="/Assessment/Record" className={({ isActive, isPending }) => isPending ? "flex p-2 text-xl flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? `flex text-xl flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl ${setIsAssignmentOpen(true)}`: "text-gray-500"}><p className="hover:text-[#B32073] w-full p-2 rounded hover:bg-gray-300 ">Record</p></NavLink>
+                            <NavLink to="/Assessment/List" className={({ isActive, isPending }) => isPending ? "flex text-xl hover:text-[#B32073] flex-row items-center duration-300 hover:shadow-xl hover:scale-105   pr-6" : isActive ? `flex text-xl flex-row items-center duration-300 scale-105 bg-gray-300 text-[#B32073] rounded hover:text-[#B32073] hover:shadow-xl ${setIsAssignmentOpen(true)}` : "text-gray-500"}><p className="hover:text-[#B32073] w-full p-2 rounded hover:bg-gray-300 ">Question List</p></NavLink>
 
                         </div>
                     </div>
--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,8 +1,20 @@
+import AdminDashboard from "../Dashboard/AdminDashboard"
 
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+      <AdminDashboard />
+      <div className="ml-56 p-3 flex flex-col justify-center items-center gap-5 font-semibold text-gray-600 bg-gray-300 h-dvh" >
+        <p className="text-4xl">404</p>
+        <h1 className="text-4xl">No Information Found</h1>
+
+        <div className="mt-10">
+          <img src="/404.jpg" alt="" className="w-96 h-72 object-cover rounded-lg mix-blend-multiply"/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
