c988ff2755050cde64a7fa11bb4e8fcfd91416c6
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2024-08-16 15:13:57 +0530

    reload after login

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -53,6 +53,7 @@ const Login = () => {
         // console.log(data);
         successMessage(data.message)
         navigate("/")
+        window.location.reload()
         clearInputs()
       }
 
